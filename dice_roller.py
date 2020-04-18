998b2c09f900fcbb9998ed9811d1b49c61b60abf
edcatellan <61007278+edcatellan@users.noreply.github.com>
2020-04-18 10:49:01 -0300

    Update dice_roller.py

--- dice_roller.py
+++ dice_roller.py
@@ -13,7 +13,7 @@ for i in range(0,dice_rolls):
     print(f'You rolled a {roll}! Critical Success!')
   else:
     print(f'You rolled a {roll}')
-print(f'You have rolled a total of {dice_sum}')
+    print(f'You have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
